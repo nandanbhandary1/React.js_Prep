fb76c850b2a7d5a1dcd5cbf34370337a0bc8672a
Nandan Bhandary <nandanbhandary24@gmail.com>
2026-04-11 16:56:44 +0530

    React-hook-form

--- react-hook-form/src/App.jsx
+++ react-hook-form/src/App.jsx
@@ -1,17 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm();
 
+  async function onSubmit(data) {
+    await new Promise((resolve) => setTimeout(resolve, 5000))
+    console.log("Submitting the form :", data);
+  }
   return (
     <>
-      Hello Jee
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="">First Name:</label>
+          <input className={errors.fname ? 'input-error':""}type="text" {...register("fname", { required: true, minLength:{value:2, message:"Min Length not satisfied"} })} />
+          {errors.fname && <p className="error-message">{errors.fname.message}</p>}
+        </div>
+        <br />
+        <div>
+          <label htmlFor="">Middle Name:</label>
+          <input type="text" {...register("mname")} />
+        </div>
+        <br />
+        <div>
+          <label htmlFor="">Last Name:</label>
+          <input type="text" {...register("lname", { pattern:{value: /^[A-Za-z]+$/i, message:"Last name is not as per rule"} })} />
+          {errors.lname && <p className="error">{errors.lname.message}</p>}
+        </div>
+        <br />
+        <input type="submit" disabled={isSubmitting} value={isSubmitting ? "Submitting" : ''}/>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
