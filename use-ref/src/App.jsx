17257c27a569d68e3f3596161e967e68277f8d2c
Nandan Bhandary <nandanbhandary24@gmail.com>
2026-04-12 16:08:53 +0530

    Finished useref

--- use-ref/src/App.jsx
+++ use-ref/src/App.jsx
@@ -1,13 +1,42 @@
-
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { useRef } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  // let val = 1;
+  let val = useRef(0);
+
+  let btnref = useRef()
+
+  function handleIncrement() {
+    // val = val + 1; // not persistent so,
+    val.current = val.current + 1;
+    // console.log(val)
+    console.log(val.current);
+    setCount(count + 1);
+  }
+
+  function changeColor() {
+    btnref.current.style.backgroundColor = "red";
+  }
+
+  //it runs on every render
+  useEffect(() => {
+    console.log("I rendered again");
+  });
 
   return (
     <>
-      
+      <button ref={btnref}onClick={handleIncrement}>Increment</button>
+      <br />
+      <button onClick={changeColor}>
+        Change Colour of 1st btn
+      </button>
+      <div>Count: {count}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
