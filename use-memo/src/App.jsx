f97e75de2cd449c4e3677ded87ae7649e84c6762
Nandan Bhandary <nandanbhandary24@gmail.com>
2026-04-12 19:16:57 +0530

    use-memo

--- use-memo/src/App.jsx
+++ use-memo/src/App.jsx
@@ -1,17 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { useMemo } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState(0);
+
+  function exp_task(num) {
+    console.log("inside exp")
+    for (let i = 0; i <= 10000000; i++) {}
+    return num * 2;
+  }
+
+  // let dvalue = exp_task(input)
+
+  let dvalue = useMemo(() => exp_task(input), [input])
 
   return (
     <>
-      Hello Jee
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <div>Count: {count}</div>
+
+      <div>
+        Double: {dvalue}
+      </div>
+
+      <input type="number" placeholder="Enter number" value={input} onChange={(e) => setInput(e.target.value)} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
